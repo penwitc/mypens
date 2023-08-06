577027800aafbef6cb550385ec65b41d0ca4bee5
penwitc <141511664+penwitc@users.noreply.github.com>
2023-08-06 12:08:56 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
         slidesToShow: 1,
         slidesToScroll: 1,
         initialSlide: 0,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
       };
 
@@ -75,6 +75,16 @@ export default function Home() {
       </div>
     </Slider>
 </section>
+
+<div className='container'>
+   <div className='row'>
+      <div className='col-sm-12 text-center'>
+      <video src="/assets/vi.mp4" autoPlay loop soun muted type="video/mp4"></video>
+      <br></br>
+      <a className='calll' href="tel:7799680585">अभी कॉल करे: 7799680585</a>
+      </div>
+   </div>
+</div>
 
             <div class="ship_section layout_padding">
                 <div class="container">
@@ -106,9 +116,7 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div class="seemore_main">
-                        <div class="seemore_bt"><a href="tel:7799680585">Call: 7799680585</a></div>
-                    </div>
+                    
                 </div>
             </div>
             <div class="rrrt">
@@ -122,29 +130,30 @@ export default function Home() {
                             <p>
                                 10 Pencil = 1 Box<br />
                                 Packing Profit 1 Box = 6 ₹
+                                <br></br>
+                                <br></br>
+                                <br></br>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="contact_section layout_padding">
-                <div class="container">
-                    <h1 class="requst_text">Requst A call for You</h1>
-                    <div class="contact_section_2">
-                        <div class="row">
-                            <div class="col-md-6 padding_0">
-                                <div class="address_main">
-                                    <div class="border_dotted">
-                                        <div class="map_text"><img src="assets/images/map-icon.png" /><span class="map_icon">Address : 702 Western Express Highway, Sir Mathuradas Vasanji Rd, Junction, Mumbai, Maharashtra 400069</span></div>
-                                        <div class="map_text_2"><img src="assets/images/call-icon.png" /><span class="map_icon">Phone : 7799680585</span></div>
-                                        <div class="map_text_2"><img src="assets/images/email-icon.png" /><span class="map_icon">Email : natrajpen@gmail.com</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           
+            <script async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'></script>
+            <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              var wa_btnSetting = {"btnColor":"#16BE45","ctaText":"WhatsApp Us","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"right","whatsAppNumber":"917799680585","welcomeMessage":"Chat Us On WhatsApp","zIndex":999999,"btnColorScheme":"light"};
+window.onload = () => {
+_waEmbed(wa_btnSetting);
+};
+          `,
+            }}
+          />
+
+
+<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-8653c816-e7ea-4f10-b760-dc3934cc302f"></div>
     </>
   )
 }
